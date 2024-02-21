7adcd3c135a2fe1332a3ae9e29874d592981ef52
Raph2ll <raphael.ph2ll@gmail.com>
2024-02-21 18:07:26 -0300

    feat: Data Annotations

--- api/Model/ProductModel.cs
+++ api/Model/ProductModel.cs
@@ -1,14 +1,16 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
 
 namespace api.Model
 {
     public class ProductModel
     {
+        [Required]
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public double Price { get; set; }
     }
 
