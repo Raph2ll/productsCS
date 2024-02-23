c3aace5889eb73294b6d68502984da6448aeb487
Raph2ll <raphael.ph2ll@gmail.com>
2024-02-23 17:06:51 -0300

    fix: service

--- api/Service/ProductService.cs
+++ api/Service/ProductService.cs
@@ -4,92 +4,36 @@ using MySql.Data.MySqlClient;
 using api.Data;
 using api.Model;
 using System.Data.Common;
+using api.Storage;
 
 public class ProductService
 {
-    private readonly Connection _connection;
+    private readonly IProductRepository _storage;
 
-    public ProductService(Connection connection)
+    public ProductService(IProductRepository storage)
     {
-        _connection = connection;
+        _storage = storage;
     }
 
     public List<ProductModel> GetAllProducts()
     {
-        var products = new List<ProductModel>();
-
-        using (var dbConnection = _connection.GetConnection())
-        {
-            dbConnection.Open();
-            using (var command = new MySqlCommand($"SELECT * FROM store.products", dbConnection))
-            {
-                using (var reader = command.ExecuteReader())
-                {
-                    while (reader.Read())
-                    {
-                        var product = new ProductModel
-                        {
-                            Id = Convert.ToInt32(reader["Id"]),
-                            Name = reader["Name"].ToString(),
-                            Price = reader["Price"] == DBNull.Value ? 0.0 : Convert.ToDouble(reader["Price"])
-                        };
-                        product.Price = Math.Round(product.Price, 2);
-
-                        products.Add(product);
-                    }
-                }
-            }
-            dbConnection.Close();
-        }
-
-        return products;
-
+        return _storage.GetAll();
     }
     public void CreateProducts(List<ProductModel> newProducts)
     {
-        using (var dbConnection = _connection.GetConnection())
-        {
-            dbConnection.Open();
-            foreach (var newProduct in newProducts)
-            {
-                using (var command = new MySqlCommand("INSERT INTO store.products(Name, Price) VALUES (@Name, @Price)", dbConnection))
-                {
-                    command.Parameters.AddWithValue("@Name", newProduct.Name);
-                    command.Parameters.AddWithValue("@Price", newProduct.Price);
-
-                    command.ExecuteNonQuery();
-                }
-            }
-            dbConnection.Close();
-        }
+        _storage.Create(newProducts);
+        // return newProducts;
+    }
+    public void UpdateProduct(ProductModel updatedProduct)
+    {
+        _storage.Update(updatedProduct);
+        // return updatedProduct;
     }
     public void DeleteProductById(int productId)
     {
-        using (var dbConnection = _connection.GetConnection())
-        {
-            dbConnection.Open();
-            using (var command = new MySqlCommand("DELETE FROM store.products WHERE Id = @ProductId", dbConnection))
-            {
-                command.Parameters.AddWithValue("@ProductId", productId);
-                command.ExecuteNonQuery();
-            }
-        }
+        _storage.Delete(productId);
+        // return _storage.GetById(productId);
     }
 
-    public void UpdateProduct(ProductModel updatedProduct)
-    {
-        using (var dbConnection = _connection.GetConnection())
-        {
-            dbConnection.Open();
-            using (var command = new MySqlCommand("UPDATE store.products SET Name = @Name, Price = @Price WHERE Id = @Id", dbConnection))
-            {
-                command.Parameters.AddWithValue("@Id", updatedProduct.Id);
-                command.Parameters.AddWithValue("@Name", updatedProduct.Name);
-                command.Parameters.AddWithValue("@Price", updatedProduct.Price);
-
-                command.ExecuteNonQuery();
-            }
-        }
-    }
 
 }
