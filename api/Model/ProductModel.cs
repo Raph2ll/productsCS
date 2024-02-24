72143b1ecba43cace7fd6b0744166ec4c97c4789
Raph2ll <raphael.ph2ll@gmail.com>
2024-02-24 15:41:22 -0300

    feat: Model DataAnnotatons

--- api/Model/ProductModel.cs
+++ api/Model/ProductModel.cs
@@ -6,11 +6,13 @@ namespace api.Model
 {
     public class ProductModel
     {
-        [Required]
         public int Id { get; set; }
         [Required]
+        [RegularExpression(@"^[a-zA-Z''-'\s]{1,40}$", ErrorMessage =
+            "Numbers and special characters are not allowed in the name.")]
         public required string Name { get; set; }
         [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "The Price field must be greater than 0.")]
         public double Price { get; set; }
     }
 
