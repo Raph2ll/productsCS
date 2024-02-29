adf2c8600792fa962a3497940f9e9840fbd05714
Raph2ll <raphael.ph2ll@gmail.com>
2024-02-29 16:06:22 -0300

    test: GetAllProducts

--- tests/ProductControllerTests.cs
+++ tests/ProductControllerTests.cs
@@ -1,18 +1,37 @@
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using api.Storage;
+using api.Controller;
+using api.Model;
 
 namespace tests
 {
     [TestClass]
     public class ProductControllerTests
     {
+        // Get All Products
         [TestMethod]
-        public void TestMethod1()
+        public void GetAllProducts_ReturnsListOfProducts()
         {
-            Assert.IsTrue(true);
+            // Arrange
+            var productRepositoryMock = new Mock<IProductRepository>();
+            var productServiceMock = new Mock<ProductService>(productRepositoryMock.Object);
+
+            productServiceMock.Setup(x => x.GetAllProducts()).Returns(new List<ProductModel>());
+            var productController = new ProductController(productServiceMock.Object);
+
+            // Act
+            var result = productController.GetAllProducts().Result;
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(OkObjectResult));
+            var okResult = (OkObjectResult)result;
+
+            Assert.IsNotNull(okResult.Value);
+            Assert.IsInstanceOfType(okResult.Value, typeof(List<ProductModel>));
         }
     }
 }
